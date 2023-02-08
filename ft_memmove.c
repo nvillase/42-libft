599a3849efb360049681ac6cde00a2a1500f1560
Noan Villaseque <nvillase@c2r3p7.42perpignan.fr>
2023-02-08 16:38:23 +0100

    cp fini

--- ft_memmove.c
+++ ft_memmove.c
@@ -1,30 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nvillase <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/07 11:36:48 by nvillase          #+#    #+#             */
-/*   Updated: 2023/02/08 14:37:49 by nvillase         ###   ########.fr       */
+/*   Created: 2023/02/08 16:32:56 by nvillase          #+#    #+#             */
+/*   Updated: 2023/02/08 16:32:58 by nvillase         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
 
-void	*ft_memcpy(void *dest, const void *src, size_t n)
-{
-	size_t		i;
-	char		*d;
-	const char	*s;
-
-	d = dest;
-	s = src;
-	i = 0;
-	while (i < n)
-	{
-		d[i] = s[i];
-		i++;
-	}
-	return (dest);
-}
