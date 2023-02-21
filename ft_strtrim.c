1cebe0e4d0f9278e07aab829b1e7f6b7b1ddc5a6
Noan Villaseque <nvillase@c2r8p4.42perpignan.fr>
2023-02-21 17:33:05 +0100

    strtrim fini

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -16,14 +16,18 @@ char	*ft_strtrim(char const *s1, char const *set)
 {
 	char	*s2;
 	size_t	debut;
-	size_t	fin;
 	size_t	len;
 
 	debut = 0;
 	len = ft_strlen(s1);
-	fin = len - 1;
-
-	while (debut <= len && ft_strchr(set, s1[debut]) != NULL)
+	while (debut < len && ft_strchr(set, s1[debut]) != NULL)
 		debut++;
-	while (fin )
+	while (len > debut && ft_strchr(set, s1[len - 1]) != NULL)
+		len--;
+	s2 = (char *)malloc(sizeof(char) * (len - debut + 1));
+	if (!s2)
+		return (NULL);
+	ft_memcpy(s2, s1 + debut, len - debut);
+	s2[len - debut] = '\0';
+	return (s2);
 }
